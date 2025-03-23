43702ad71c1e7e2aed4270a96bcf9dc6e666d05b
NikoNiiOS <ryxxal@gmail.com>
2025-03-23 13:52:45 +0100

    Stripe update

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -3,16 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Stripe\Stripe;
+
+use App\Service\StripeService;
 
 class PaymentController extends AbstractController
 {
-    #[Route('/payment', name: 'app_payment')]
-    public function index(): Response
+    #[Route('/stripe', name: 'app_stripe')]
+    public function index(Request $request, StripeService $stripeService): RedirectResponse
     {
-        return $this->render('payment/index.html.twig', [
-            'controller_name' => 'PaymentController',
-        ]);
+        Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
+
+        $totalAmount = $request->request->get('totalAmount');
+
+        $checkout_session = $stripeService->checkout($totalAmount);
+
+        return new RedirectResponse($checkout_session->url);
+    }
+
+    #[Route('/success', name: 'stripe_success')]
+    public function success() {
+        return $this->render('stripe/success.html.twig');
+    }
+
+    #[Route('/cancel', name: 'stripe_cancel')]
+    public function cancel() {
+        return $this->render('stripe/cancel.html.twig');
     }
 }
